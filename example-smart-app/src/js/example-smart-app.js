1ee2d0f0346f44bda316a3340f750ff55bc288fc
davemateer <davemateer@users.noreply.github.com>
2021-05-14 10:56:41 -0400

    Fix bug with pseudo-array join

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,8 +32,8 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+            fname = Array.prototype.join.call(patient.name[0].given, ' ');
+            lname = Array.prototype.join.call(patient.name[0].family, ' ');
           }
 
           var height = byCodes('8302-2');
